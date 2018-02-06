881e6cac775e50e979b0f511bbe4ba4ee80731b2
Adrian Osmond <adrian@Adrians-MacBook-Pro.local>
2018-02-06 16:12:15 +0100

    Game sort of works

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Cube from './components/Cube'
 
 import './App.css';
 
+
+const NUM_OPTIONS = 5;
 const colors = ['red', 'blue', 'green', 'gold', 'deepskyblue', 'magenta']
 
 const shuffle = (array) => {
@@ -17,6 +19,33 @@ const shuffle = (array) => {
   return ret
 }
 
+const orientationVisibles = [[],
+  [5,4,3], [5,2,4], [5,3,1], [5,1,2],
+  [0,2,1], [0,1,3], [0,4,2], [0,3,4],
+  [2,0,4], [2,4,5], [2,1,0], [2,5,1],
+  [3,1,5], [3,0,1], [3,5,4], [3,4,0],
+  [4,0,3], [4,2,0], [4,3,5], [4,5,2],
+  [1,2,5], [1,5,3], [1,0,2], [1,3,0]
+]
+
+const getVisibleColors = (solution, orientation) => {
+  const visibles = orientationVisibles[orientation]
+  return visibles.map(v => solution[v]);
+}
+
+const solutionsClash = (solution, orientation, potentialClash) => {
+  const solutionColors = getVisibleColors(solution, orientation)
+  for (let i=1; i<=24; i++) {
+    let clashColors = getVisibleColors(potentialClash, i)
+    if (solutionColors[0] === clashColors[0] &&
+        solutionColors[1] === clashColors[1] &&
+        solutionColors[2] === clashColors[2]) {
+      return true
+    }
+  }
+  return false
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -25,31 +54,45 @@ class App extends Component {
 
   generatePuzzle() {
     const solution = shuffle(colors)
-    const decoy1 = shuffle(colors)
-    const decoy2 = shuffle(colors)
-    const decoy3 = shuffle(colors)
-    const decoy4 = shuffle(colors)
-    const options = shuffle([solution, decoy1, decoy2, decoy3, decoy4])
+    const orientation = Math.ceil(Math.random() * 24)
+    let options = [solution]
+    while (options.length < NUM_OPTIONS) {
+      let decoy = shuffle(colors);
+      if (!solutionsClash(solution, orientation, decoy)) {
+        options.push(decoy)
+      }
+    }
+
+    options = shuffle(options)
     return {
       solution,
-      orientation: Math.round(Math.random() * 24),
-      options
+      orientation,
+      options,
+      chosen: false
     }
   }
 
+  makeSelection(cube) {
+    this.setState({
+      chosen: true
+    })
+
+    // setTimeout(() => {
+    //   this.setState(this.generatePuzzle())
+    // }, 3000)
+  }
+
   render() {
-    console.log(this.state)
     return (
       <div className="App">
         <div className="solution">
-          <Cube colors={this.state.solution} orientation={this.state.orientation} />
+          <Cube colors={this.state.solution} orientation={this.state.orientation} chosen={this.state.chosen} />
         </div>
         <ul className="options">
           {this.state.options.map((cube, idx)=>{
             return (
-              <li className="options__choice" key={idx}>
+              <li className={`options__choice${this.state.chosen && cube === this.state.solution ? ' options__choice--correct' : ''}`} key={idx} onClick={this.makeSelection.bind(this, cube)}>
                 <Cube colors={cube} exploded={true} />
-                {/* {cube === this.state.solution ? <p>XXX</p> : null} */}
               </li>
             )
           })}
--- src/components/Cube/index.js
+++ src/components/Cube/index.js
@@ -4,13 +4,15 @@ import './index.css'
 
 const Cube = (props) => {
   return (
-    <div className={`cube${ props.exploded ? ' cube--exploded' : '' }${ props.orientation ? ' cube--orientation-' + props.orientation : '' }`}>
-      <div className="cube__face cube__face--1" style={{backgroundColor: props.colors[0]}}/>
-      <div className="cube__face cube__face--2" style={{backgroundColor: props.colors[1]}}/>
-      <div className="cube__face cube__face--3" style={{backgroundColor: props.colors[2]}}/>
-      <div className="cube__face cube__face--4" style={{backgroundColor: props.colors[3]}}/>
-      <div className="cube__face cube__face--5" style={{backgroundColor: props.colors[4]}}/>
-      <div className="cube__face cube__face--6" style={{backgroundColor: props.colors[5]}}/>
+    <div className={`cube${ props.exploded ? ' cube--exploded' : '' }${ props.orientation ? ' cube--orientation-' + props.orientation : '' }${ props.chosen ? ' cube--unfolded' : ''}`}>
+      <div className="cube__face cube__face--front" style={{backgroundColor: props.colors[0]}}>
+        <div className="cube__face cube__face--top" style={{backgroundColor: props.colors[1]}}/>
+        <div className="cube__face cube__face--left" style={{backgroundColor: props.colors[2]}}/>
+        <div className="cube__face cube__face--right" style={{backgroundColor: props.colors[3]}}/>
+        <div className="cube__face cube__face--bottom" style={{backgroundColor: props.colors[4]}}>
+          <div className="cube__face cube__face--back" style={{backgroundColor: props.colors[5]}}/>
+        </div>
+      </div>
     </div>
   )
 }
